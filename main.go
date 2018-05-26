72aaa480456151f401133d8eaa52a8a41d2dea3f
Jared Lindauer <jared@Jareds-MacBook-Air.local>
2018-05-26 17:16:44 -0400

    Chapter 6.1

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "github.com/gorilla/mux"
+  "text/template"
+)
+
+var homeTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html")
+  if err := homeTemplate.Execute(w, nil); err != nil {
+    panic(err)
+  }
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "To get in touch, please send an email "+
+    "to <a href=\"mailto:support@lenslocked.com\">"+
+    "support@lenslocked.com</a>.")
+}
+
+func main() {
+  var err error
+  homeTemplate, err = template.ParseFiles("views/home.gohtml")
+  if err != nil {
+    panic(err)
+  }
+
+  r := mux.NewRouter()
+  r.HandleFunc("/", home)
+  r.HandleFunc("/contact", contact)
+  http.ListenAndServe(":3000", r)
+}

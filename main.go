0b9662ef5c22e2b09158be5513288b6fa11ac70e
Jared Lindauer <jslindauer17@gmail.com>
2018-05-27 18:12:53 -0400

    Chapter 7.3

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
   "net/http"
   "github.com/gorilla/mux"
   "github.com/jlindauer/usegolang/views"
+  "github.com/jlindauer/usegolang/controllers"
 )
 
 var (
   homeView    *views.View
   contactView *views.View
-  signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -22,11 +22,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
   must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  must(signupView.Render(w, nil))
-}
-
 func must(err error) {
   if err != nil {
     panic(err)
@@ -36,11 +31,11 @@ func must(err error) {
 func main() {
   homeView = views.NewView("bootstrap", "views/home.gohtml")
   contactView = views.NewView("bootstrap", "views/contact.gohtml")
-  signupView = views.NewView("bootstrap", "views/signup.gohtml")
+  usersC := controllers.NewUsers()
 
   r := mux.NewRouter()
   r.HandleFunc("/", home)
   r.HandleFunc("/contact", contact)
-  r.HandleFunc("/signup", signup)
+  r.HandleFunc("/signup", usersC.New)
   http.ListenAndServe(":3000", r)
 }

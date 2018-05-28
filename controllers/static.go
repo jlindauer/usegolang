b02e01f19f13a6baee4cee042b8659263935ac9a
Jared Lindauer <jslindauer17@gmail.com>
2018-05-28 16:40:19 -0400

    Chapter 7.6.1

--- controllers/static.go
+++ controllers/static.go
@@ -7,10 +7,12 @@ func NewStatic() *Static {
   return &Static{
     Home: views.NewView("bootstrap", "static/home"),
     Contact: views.NewView("bootstrap", "static/contact"),
+    FAQ: views.NewView("bootstrap", "static/faq"),
   }
 }
 
 type Static struct {
   Home *views.View
   Contact *views.View
+  FAQ *views.View
 }
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
   r := mux.NewRouter()
   r.Handle("/", staticC.Home).Methods("GET")
   r.Handle("/contact", staticC.Contact).Methods("GET")
+  r.Handle("/faq", staticC.FAQ).Methods("GET")
   r.HandleFunc("/signup", usersC.New).Methods("GET")
   r.HandleFunc("/signup", usersC.Create).Methods("POST")
   http.ListenAndServe(":3000", r)

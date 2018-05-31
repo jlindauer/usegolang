c688bf7ded373dd5b58c8268b1c48eafb34a03e1
Jared Lindauer <jslindauer17@gmail.com>
2018-05-30 21:31:28 -0400

    Chapter 8.3.6

--- exp/main.go
+++ exp/main.go
@@ -24,20 +24,23 @@ func main() {
 	}
 
 	var id int
-	var name, email string
-	rows, err := db.Query(`
-    SELECT id, name, email
-    FROM users
-    WHERE email=$1
-		OR ID > $2`,
-		"jon@calhoun.io", 3)
-	if err != nil {
-		panic(err)
-	}
+	for i := 1; i < 6; i++ {
+		userId := 1
+		if i > 3 {
+			userId = 2
+		}
+		amount := 1000 * i
+		description := fmt.Sprintf("USB-C Adapter x%d", i)
 
-	for rows.Next() {
-		rows.Scan(&id, &name, &email)
-		fmt.Println("ID:", id, "Name:", name, "Email:", email)
+		err = db.QueryRow(`
+			INSERT INTO orders (user_id, amount, description)
+			VALUES ($1, $2, $3)
+			RETURNING id`,
+			userId, amount, description).Scan(&id)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Created an order with the ID:", id)
 	}
 
 	db.Close()

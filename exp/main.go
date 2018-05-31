d7dcd2f07fe32ed34d7448adb11952d6b88af747
Jared Lindauer <jslindauer17@gmail.com>
2018-05-30 21:26:22 -0400

    Chapter 8.3.5

--- exp/main.go
+++ exp/main.go
@@ -22,22 +22,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = db.Ping()
+
+	var id int
+	var name, email string
+	rows, err := db.Query(`
+    SELECT id, name, email
+    FROM users
+    WHERE email=$1
+		OR ID > $2`,
+		"jon@calhoun.io", 3)
 	if err != nil {
 		panic(err)
 	}
 
-	var id int
-	var name, email string
-	row := db.QueryRow(`
-    SELECT id, name, email
-    FROM users
-    WHERE id=$1`, 1)
-	err = row.Scan(&id, &name, &email)
-	if err != nil {
-		panic(err)
+	for rows.Next() {
+		rows.Scan(&id, &name, &email)
+		fmt.Println("ID:", id, "Name:", name, "Email:", email)
 	}
-	fmt.Println("ID:", id, "Name:", name, "Email:", email)
 
 	db.Close()
 }

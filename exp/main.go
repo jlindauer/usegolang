7ed787b1aaeaca89965c4ff4bbbbc785f62d5f0e
Jared Lindauer <jslindauer17@gmail.com>
2018-05-30 22:10:17 -0400

    Chapter 8.4.2

--- exp/main.go
+++ exp/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+
+	"github.com/jinzhu/gorm"
 
 	_ "github.com/lib/pq"
 )
@@ -15,33 +16,19 @@ const (
 	dbname   = "usegolang_dev"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable",
 		dbname, user, password, host, port)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
-	var id int
-	for i := 1; i < 6; i++ {
-		userId := 1
-		if i > 3 {
-			userId = 2
-		}
-		amount := 1000 * i
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-
-		err = db.QueryRow(`
-			INSERT INTO orders (user_id, amount, description)
-			VALUES ($1, $2, $3)
-			RETURNING id`,
-			userId, amount, description).Scan(&id)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Created an order with the ID:", id)
-	}
-
-	db.Close()
+	defer db.Close()
 }

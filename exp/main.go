3f5a96ce05bc93eae8278232b694c771222fda49
Jared Lindauer <jslindauer17@gmail.com>
2018-05-30 20:51:09 -0400

    Chapter 8.3.4

--- exp/main.go
+++ exp/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
-  "html/template"
-  "os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "usegolang_dev"
 )
 
 func main() {
-  t, err := template.ParseFiles("hello.gohtml")
-  if err != nil {
-    panic(err)
-  }
+	psqlInfo := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable",
+		dbname, user, password, host, port)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
-  data := struct {
-    Name string
-  }{"<script>alert('Howdy!');</script>"}
+	var id int
+	var name, email string
+	row := db.QueryRow(`
+    SELECT id, name, email
+    FROM users
+    WHERE id=$1`, 1)
+	err = row.Scan(&id, &name, &email)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ID:", id, "Name:", name, "Email:", email)
 
-  err = t.Execute(os.Stdout, data)
-  if err != nil {
-    panic(err)
-  }
+	db.Close()
 }

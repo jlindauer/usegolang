6281415664fe453763b3f3defb620d265be9aeb4
Jared Lindauer <jslindauer17@gmail.com>
2018-06-05 16:36:29 -0400

    Chapter 9.2

--- exp/main.go
+++ exp/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinzhu/gorm"
-
+	"github.com/jlindauer/usegolang/models"
 	_ "github.com/lib/pq"
 )
 
@@ -16,39 +15,19 @@ const (
 	dbname   = "usegolang_dev"
 )
 
-type User struct {
-	gorm.Model
-	Name   string
-	Email  string `gorm:"not null;unique_index"`
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable",
 		dbname, user, password, host, port)
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer us.Close()
+	us.DestructiveReset()
 
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
-
-	var user User
-	db.Preload("Orders").First(&user)
-	if db.Error != nil {
-		panic(db.Error)
+	user, err := us.ByID(1)
+	if err != nil {
+		panic(err)
 	}
-
-	fmt.Println("Email:", user.Email)
-	fmt.Println("Number of orders:", len(user.Orders))
-	fmt.Println("Orders:", user.Orders)
+	fmt.Println(user)
 }

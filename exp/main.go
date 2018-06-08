de0b52d4fc6c7570296d993cfbc727b8ad6e26bd
Jared Lindauer <jslindauer17@gmail.com>
2018-06-07 22:18:52 -0400

    Chapter 11.5

--- exp/main.go
+++ exp/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/jlindauer/usegolang/models"
-	_ "github.com/lib/pq"
+	"github.com/jlindauer/usegolang/rand"
 )
 
 const (
@@ -16,41 +15,6 @@ const (
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%d sslmode=disable",
-		dbname, user, password, host, port)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer us.Close()
-	us.DestructiveReset()
-
-	// Create a user
-	user := models.User{
-		Name:  "Michael Scott",
-		Email: "michael@dundermifflin.com",
-	}
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	user.Name = "Update Name"
-	if err := us.Update(&user); err != nil {
-		panic(err)
-	}
-
-	foundUser, err := us.ByEmail("michael@dundermifflin.com")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(foundUser)
-
-	if err := us.Delete(foundUser.ID); err != nil {
-		panic(err)
-	}
-
-	_, err = us.ByID(foundUser.ID)
-	if err != models.ErrNotFound {
-		panic("user was not deleted!")
-	}
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }

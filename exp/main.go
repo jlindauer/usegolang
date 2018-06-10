3e81ced23ca85e314c12bcfab097034841cc3840
Jared Lindauer <jslindauer17@gmail.com>
2018-06-10 13:06:50 -0400

    Chappter 11.6

--- exp/main.go
+++ exp/main.go
@@ -3,18 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/jlindauer/usegolang/rand"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = ""
-	dbname   = "usegolang_dev"
+	"github.com/jlindauer/usegolang/hash"
 )
 
 func main() {
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	hmac := hash.NewHMAC("my-secret-key")
+	fmt.Println(hmac.Hash("this is my string to hash"))
 }

ff1afa767512685552594b909a94900a09f2afde
Jared Lindauer <jslindauer17@gmail.com>
2018-08-05 15:34:11 -0400

    Chapter 13.3

--- controllers/users.go
+++ controllers/users.go
@@ -44,12 +44,22 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 		Message string
 	}
 
+	type Data struct {
+		Alert *Alert
+		Yield interface{}
+	}
+
 	alert := Alert{
 		Level:   "success",
 		Message: "Successfully rendered a dynamic alert!",
 	}
 
-	if err := u.NewView.Render(w, alert); err != nil {
+	data := Data{
+		Alert: &alert,
+		Yield: "this can be any data because its type is interface",
+	}
+
+	if err := u.NewView.Render(w, data); err != nil {
 		panic(err)
 	}
 }

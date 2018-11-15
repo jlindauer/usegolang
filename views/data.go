cfe768b58f274685890b8d24aa70859862d1cb9f
Jared Lindauer <jslindauer17@gmail.com>
2018-11-14 23:10:49 -0500

    Chapter 13.6

--- views/data.go
+++ views/data.go
@@ -1,25 +1,46 @@
 package views
 
-const (
-  AlertLvlError   = "danger"
-  AlertLvlWarning = "warning"
-  AlertLvlInfo    = "info"
-  AlertLvlSuccess = "success"
+import "log"
 
-  // AlertMsgGeneric is displayed when any random error
-  // is encountered by our backend
-  AlertMsgGeneric = "Something went wrong. Please try again, " +
-    "and contact us if the problem persists."
+const (
+	AlertLvlError   = "danger"
+	AlertLvlWarning = "warning"
+	AlertLvlInfo    = "info"
+	AlertLvlSuccess = "success"
+
+	// AlertMsgGeneric is displayed when any random error
+	// is encountered by our backend
+	AlertMsgGeneric = "Something went wrong. Please try again, " +
+		"and contact us if the problem persists."
 )
 
 // Data is the top level structure that views expect data to come in
 type Data struct {
-  Alert *Alert
-  Yield interface{}
+	Alert *Alert
+	Yield interface{}
 }
 
 // Alert is used to render Bootstrap Alert messages in template
 type Alert struct {
-  Level   string
-  Message string
+	Level   string
+	Message string
+}
+
+type PublicError interface {
+	error
+	Public() string
+}
+
+func (d *Data) SetAlert(err error) {
+	var msg string
+	if pErr, ok := err.(PublicError); ok {
+		msg = pErr.Public()
+	} else {
+		log.Println(err)
+		msg = AlertMsgGeneric
+	}
+	d.Alert = &Alert{
+		Level:   AlertLvlError,
+		Message: msg,
+	}
 }

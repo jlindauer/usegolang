ed4b477fdf17579801fe19e2cfd9f2b15e5f48c9
Jared Lindauer <jslindauer17@gmail.com>
2018-11-26 22:07:30 -0500

    Chapter 14.2

--- models/galleries.go
+++ models/galleries.go
@@ -7,3 +7,20 @@ type Gallery struct {
   UserID uint  `gorm:"not_null;index"`
   Title string `gorm:"not_null"`
 }
+
+type GalleryService interface {
+  GalleryDB
+}
+
+type GalleryDB interface {
+  Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+  db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+  // TODO: Implement this later
+  return nil
+}

6b4916638e568a4c05af09aed9c5c3fcfe712a20
Jared Lindauer <jslindauer17@gmail.com>
2019-01-26 14:24:56 -0500

    Chapter 16.4

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
   "net/http"
+  "strings"
 
   "github.com/jlindauer/usegolang/models"
   "github.com/jlindauer/usegolang/context"
@@ -21,6 +22,14 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    path := r.URL.Path
+    // If the user is requesting a static asset or image we will
+    // not need to lookup the current user so we skip doing that.
+    if strings.HasPrefix(path, "/assets/") || strings.HasPrefix(path, "/images/") {
+      next(w, r)
+      return
+    }
+
     cookie, err := r.Cookie("remember_token")
     if err != nil {
       next(w, r)
